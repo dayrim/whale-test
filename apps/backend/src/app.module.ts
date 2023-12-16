75dad30458f80fdeeb146ecf8a2f3ace08585841
Dmitrijs Pavlovs <dmitrijs.pavlovs123@gmail.com>
2023-12-16 12:46:48 +0200

    Log postgres config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -15,7 +15,7 @@ import { TelegramUser } from './telegram-user.entity';
     TelegrafModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
         const webhookDomain = configService.get<string>('TELEGRAM_WEBHOOK_DOMAIN');
-        const logger = new Logger('TelegrafModule');
+        const logger = new Logger('TelegrafConfig');
         logger.verbose(`Webhook domain is set to: ${webhookDomain}`);
 
         return {
@@ -34,10 +34,18 @@ import { TelegramUser } from './telegram-user.entity';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
+        const logger = new Logger('PostgresConfig');
+
+        const host = configService.get<string>('POSTGRES_HOST');
+        logger.verbose(`Postgres host is set to: ${host}`);
+
+        const port = configService.get<number>('POSTGRES_PORT');
+        logger.verbose(`Postgres port is set to: ${port}`);
+
         return {
           type: 'postgres',
-          host: configService.get<string>('POSTGRES_HOST'),
-          port: configService.get<number>('POSTGRES_PORT'),
+          host,
+          port,
           username: configService.get<string>('POSTGRES_USER'),
           password: configService.get<string>('POSTGRES_PASSWORD'),
           database: configService.get<string>('POSTGRES_DB'),
